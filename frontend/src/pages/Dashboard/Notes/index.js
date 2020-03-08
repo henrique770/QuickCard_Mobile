3018162dd8b2433386dee22c5b949c12d5b04e09
henrique770 <henrique.1360@gmail.com>
2020-03-08 12:02:53 -0300

    feat: config of webview react-native

--- frontend/src/pages/Dashboard/Notes/index.js
+++ frontend/src/pages/Dashboard/Notes/index.js
@@ -1,70 +1,42 @@
-import React from 'react';
+ import React from 'react';
+ import Spacing from '~/components/Spacing';
+ import {TouchableOpacity} from 'react-native';
+ import { WebView } from 'react-native-webview';
+ import IconMi from 'react-native-vector-icons/MaterialIcons';
+ import IconMc from 'react-native-vector-icons/MaterialCommunityIcons';
+ import {Container, Title, ContainerTag, TagInput, TextNote} from './styles';
+ export default function Notes({navigation}) {
+   return (
+     <>
 
-import Spacing from '~/components/Spacing';
-import {TouchableOpacity} from 'react-native';
-// import WebView from 'react-native-webview';
-
-import IconMi from 'react-native-vector-icons/MaterialIcons';
-import IconMc from 'react-native-vector-icons/MaterialCommunityIcons';
-
-import {Container, Title, ContainerTag, TagInput, TextNote} from './styles';
-
-
-export default function Notes({navigation}) {
-  return (
-    <>
-      <Container>
-        <Spacing mb="40">
-          <Spacing position="absolute" top="30" left="30">
-            <TouchableOpacity onPress={() => navigation.navigate('Dashboard')}>
-              <IconMi name="arrow-back" size={30} color="#f93b10" />
-            </TouchableOpacity>
-          </Spacing>
-          <Spacing position="absolute" top="22" right="30" width="50">
-            <ContainerTag>
-              <IconMc name="tag" size={20} color="#f93b10" />
-              <TagInput
-                autoCorrect={false}
-                autoCapitalize="none"
-                placeholder="Tag Baralho"
-              />
-            </ContainerTag>
-          </Spacing>
-        </Spacing>
+       <Container>
+         <Spacing mb="40">
+           <Spacing position="absolute" top="30" left="30">
+             <TouchableOpacity onPress={() => navigation.navigate('Dashboard')}>
+               <IconMi name="arrow-back" size={30} color="#f93b10" />
+             </TouchableOpacity>
+           </Spacing>
+           <Spacing position="absolute" top="22" right="30" width="50">
+             <ContainerTag>
+               <IconMc name="tag" size={20} color="#f93b10" />
+               <TagInput
+                 autoCorrect={false}
+                 autoCapitalize="none"
+                 placeholder="Tag Baralho"
+               />
+             </ContainerTag>
+           </Spacing>
+         </Spacing>
+           <Spacing ml="20" mr="20">
+           <Title>Título</Title>
+           </Spacing>
+           <WebView
+        originWhitelist={['*']}
+        source={{ html: '<h1 style="color: #f90; font-size: 8rem;">Hello world</h1>' }}
+      />
+       </Container>
+     </>
+   );
+ }
 
 
-
-
-          <Spacing ml="20" mr="20">
-          <Title>Título</Title>
-
-
-          <TextNote >
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur
-            porro adipisci molestiae nemo quaerat nobis, alias eum voluptas
-            modi, voluptates dicta deserunt at eaque incidunt hic, repudiandae
-            iure odio cum. Lorem ipsum dolor sit amet consectetur, adipisicing
-            elit. Pariatur porro adipisci molestiae nemo quaerat nobis, alias
-            eum voluptas modi, voluptates dicta deserunt at eaque incidunt hic,
-            repudiandae iure odio cum. modi, voluptates dicta deserunt at eaque
-            incidunt hic, repudiandae iure odio cum. Lorem ipsum dolor sit amet
-            consectetur, adipisicing elit. Pariatur porro adipisci molestiae
-            nemo quaerat nobis, alias eum voluptas modi, voluptates dicta
-            deserunt at eaque incidunt hic, repudiandae iure odio cum.
-          </TextNote>
-
-          <TextNote>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur
-            porro adipisci molestiae nemo quaerat nobis, alias eum voluptas
-            modi, voluptates dicta deserunt at eaque incidunt hic, repudiandae
-            iure odio cum. Lorem ipsum dolor sit amet consectetur, adipisicing
-            elit. Pariatur porro adipisci molestiae nemo quaerat nobis, alias
-            eum voluptas modi, voluptates dicta deserunt at eaque incidunt hic,
-            repudiandae iure odio cum.
-          </TextNote>
-          </Spacing>
-
-      </Container>
-    </>
-  );
-}
