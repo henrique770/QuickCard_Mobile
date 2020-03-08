ca6eb3f03e1aba3e9e87ddf76c77cb6bd5ad141f
henrique770 <henrique.1360@gmail.com>
2020-03-08 17:58:36 -0300

    feat: icons on toolbar

--- frontend/src/pages/Dashboard/Notes/index.js
+++ frontend/src/pages/Dashboard/Notes/index.js
@@ -1,223 +1,228 @@
- import React, {Component} from 'react';
- import Spacing from '~/components/Spacing';
- import {TouchableOpacity, StyleSheet,Text
-  , KeyboardAvoidingView, View} from 'react-native';
-  import  CNEditor , { CNToolbar,
-    // getInitialObject , CNRichTextEditor, // old editor
-    getDefaultStyles } from "react-native-cn-richtext-editor";
+import React, {Component} from 'react';
+import Spacing from '~/components/Spacing';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+  View,
+} from 'react-native';
 
- import IconMi from 'react-native-vector-icons/MaterialIcons';
- import IconMc from 'react-native-vector-icons/MaterialCommunityIcons';
- import {Container, Title, ContainerTag, TagInput, TextNote} from './styles';
+import CNEditor, {
+  CNToolbar,
+  getDefaultStyles,
+} from 'react-native-cn-richtext-editor';
 
- const defaultStyles = getDefaultStyles();
+import IconMi from 'react-native-vector-icons/MaterialIcons';
+import IconMc from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Container, Title, ContainerTag, TagInput} from './styles';
 
- export default class Notes extends Component {
+const defaultStyles = getDefaultStyles();
 
+export default class Notes extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        selectedTag : 'body',
-        selectedStyles : [],
+      selectedTag: 'body',
+      selectedStyles: [],
     };
 
     this.editor = null;
-}
+  }
 
-onStyleKeyPress = (toolType) => {
+  onStyleKeyPress = toolType => {
     this.editor.applyToolbar(toolType);
-}
+  };
 
-onSelectedTagChanged = (tag) => {
+  onSelectedTagChanged = tag => {
     this.setState({
-        selectedTag: tag
-    })
-}
+      selectedTag: tag,
+    });
+  };
 
-onSelectedStyleChanged = (styles) => {
+  onSelectedStyleChanged = styles => {
     this.setState({
-        selectedStyles: styles,
-    })
-}
+      selectedStyles: styles,
+    });
+  };
 
-render() {
+  render() {
+    return (
+      <>
+        <Container>
+          <Spacing mb="40">
+            <Spacing position="absolute" top="30" left="30">
+              <TouchableOpacity
+                onPress={() => this.props.navigation.navigate('Dashboard')}>
+                <IconMi name="arrow-back" size={30} color="#f93b10" />
+              </TouchableOpacity>
+            </Spacing>
+            <Spacing position="absolute" top="22" right="30" width="50">
+              <ContainerTag>
+                <IconMc name="tag" size={20} color="#f93b10" />
+                <TagInput
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                  placeholder="Tag Baralho"
+                />
+              </ContainerTag>
+            </Spacing>
+          </Spacing>
+          <Spacing ml="20" mr="20">
+            <Title>Título</Title>
+          </Spacing>
 
-   return (
-     <>
-       <Container>
-         <Spacing mb="40">
-           <Spacing position="absolute" top="30" left="30">
-             <TouchableOpacity onPress={() => this.props.navigation.navigate('Dashboard')}>
-               <IconMi name="arrow-back" size={30} color="#f93b10" />
-             </TouchableOpacity>
-           </Spacing>
-           <Spacing position="absolute" top="22" right="30" width="50">
-             <ContainerTag>
-               <IconMc name="tag" size={20} color="#f93b10" />
-               <TagInput
-                 autoCorrect={false}
-                 autoCapitalize="none"
-                 placeholder="Tag Baralho"
-               />
-             </ContainerTag>
-           </Spacing>
-         </Spacing>
-           <Spacing ml="20" mr="20">
-           <Title>Título</Title>
-           </Spacing>
-
-           <KeyboardAvoidingView
+          <KeyboardAvoidingView
             enabled
             keyboardVerticalOffset={0}
             style={{
-                flex: 1,
-                paddingTop: 20,
-                backgroundColor:'#fff',
-                flexDirection: 'column',
-                justifyContent: 'flex-end',
-            }}
-            >
-                <View
-                style={{flex: 1}}
-                onTouchStart={() => {
-                   this.editor && this.editor.blur();
-                }}
-                >
-                   <View style={styles.main}
-                    onTouchStart={(e) => e.stopPropagation()}>
-
-                        <CNEditor
-                          ref={input => this.editor = input}
-                          onSelectedTagChanged={this.onSelectedTagChanged}
-                          onSelectedStyleChanged={this.onSelectedStyleChanged}
-                          style={{ backgroundColor : '#fff'}}
-                          styleList={defaultStyles}
-                          initialHtml={`
+              flex: 1,
+              backgroundColor: '#fff',
+              flexDirection: 'column',
+              justifyContent: 'flex-end',
+            }}>
+            <View
+              style={{flex: 1}}
+              onTouchStart={() => {
+                this.editor && this.editor.blur();
+              }}>
+              <View style={styles.main} onTouchStart={e => e.stopPropagation()}>
+                <CNEditor
+                  ref={input => (this.editor = input)}
+                  onSelectedTagChanged={this.onSelectedTagChanged}
+                  onSelectedStyleChanged={this.onSelectedStyleChanged}
+                  style={{backgroundColor: '#fff'}}
+                  styleList={defaultStyles}
+                  initialHtml={`
                           <h1>HTML Ipsum Presents</h1>
                             <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
                             `}
-                        />
-                    </View>
-                </View>
+                />
+              </View>
+            </View>
 
-                <View style={{
-                    minHeight: 35
-                }}>
-
-                    <CNToolbar
-                                style={{
-                                    height: 35,
-                                }}
-                                iconSetContainerStyle={{
-                                    flexGrow: 1,
-                                    justifyContent: 'space-evenly',
-                                    alignItems: 'center',
-                                }}
-                                size={30}
-                                iconSet={[
-                                    {
-                                        type: 'tool',
-                                        iconArray: [{
-                                            toolTypeText: 'image',
-                                            iconComponent:
-                                                <Text style={styles.toolbarButton}>
-                                                image
-                                                </Text>
-                                        }]
-                                    },
-                                    {
-                                        type: 'tool',
-                                        iconArray: [{
-                                            toolTypeText: 'bold',
-                                            buttonTypes: 'style',
-                                            iconComponent:
-                                                <Text style={styles.toolbarButton}>
-                                                bold
-                                                </Text>
-                                        }]
-                                    },
-                                    {
-                                        type: 'seperator'
-                                    },
-                                    {
-                                        type: 'tool',
-                                        iconArray: [
-                                            {
-                                                toolTypeText: 'body',
-                                                buttonTypes: 'tag',
-                                                iconComponent:
-                                                    <Text style={styles.toolbarButton}>
-                                                    body
-                                                    </Text>
-                                            },
-                                        ]
-                                    },
-                                    {
-                                        type: 'tool',
-                                        iconArray: [
-                                            {
-                                                toolTypeText: 'ul',
-                                                buttonTypes: 'tag',
-                                                iconComponent:
-                                                    <Text style={styles.toolbarButton}>
-                                                    ul
-                                                    </Text>
-                                            }
-                                        ]
-                                    },
-                                    {
-                                        type: 'tool',
-                                        iconArray: [
-                                            {
-                                                toolTypeText: 'ol',
-                                                buttonTypes: 'tag',
-                                                iconComponent:
-                                                    <Text style={styles.toolbarButton}>
-                                                    ol
-                                                    </Text>
-                                            }
-                                        ]
-                                    },
-                                ]}
-                                selectedTag={this.state.selectedTag}
-                                selectedStyles={this.state.selectedStyles}
-                                onStyleKeyPress={this.onStyleKeyPress}
-                            />
-                </View>
-        </KeyboardAvoidingView>
-
-
-
-
-       </Container>
-     </>
-   )};
- }
- var styles = StyleSheet.create({
+            <View
+              style={{
+                minHeight: 35,
+              }}>
+              <CNToolbar
+                style={{
+                  height: 45,
+                }}
+                iconSetContainerStyle={{
+                  flexGrow: 1,
+                  justifyContent: 'space-evenly',
+                  alignItems: 'center',
+                }}
+                size={30}
+                iconSet={[
+                  {
+                    type: 'tool',
+                    iconArray: [
+                      {
+                        buttonTypes: 'style',
+                        toolTypeText: 'image',
+                        iconComponent: (
+                          <Text style={styles.toolbarButton}>
+                            <IconMi name="image" size={30} />
+                          </Text>
+                        ),
+                      },
+                    ],
+                  },
+                  {
+                    type: 'tool',
+                    iconArray: [
+                      {
+                        toolTypeText: 'bold',
+                        buttonTypes: 'style',
+                        iconComponent: (
+                          <Text style={styles.toolbarButton}>
+                            <IconMi name="format-bold" size={30} />
+                          </Text>
+                        ),
+                      },
+                    ],
+                  },
+                  {
+                    type: 'tool',
+                    iconArray: [
+                      {
+                        toolTypeText: 'italic',
+                        buttonTypes: 'tag',
+                        iconComponent: (
+                          <Text style={styles.toolbarButton}>
+                            <IconMi name="format-italic" size={30} />
+                          </Text>
+                        ),
+                      },
+                    ],
+                  },
+                  {
+                    type: 'tool',
+                    iconArray: [
+                      {
+                        toolTypeText: 'ul',
+                        buttonTypes: 'tag',
+                        iconComponent: (
+                          <Text style={styles.toolbarButton}>
+                            <IconMi name="format-list-bulleted" size={30} />
+                          </Text>
+                        ),
+                      },
+                    ],
+                  },
+                  {
+                    type: 'tool',
+                    iconArray: [
+                      {
+                        toolTypeText: 'ol',
+                        buttonTypes: 'tag',
+                        iconComponent: (
+                          <Text style={styles.toolbarButton}>
+                            <IconMi name="format-list-numbered" size={30} />
+                          </Text>
+                        ),
+                      },
+                    ],
+                  },
+                ]}
+                selectedTag={this.state.selectedTag}
+                selectedStyles={this.state.selectedStyles}
+                onStyleKeyPress={this.onStyleKeyPress}
+              />
+            </View>
+          </KeyboardAvoidingView>
+        </Container>
+      </>
+    );
+  }
+}
+var styles = StyleSheet.create({
   main: {
-      flex: 1,
-      paddingLeft: 15,
-      paddingRight: 15,
-      paddingBottom: 1,
-      alignItems: 'stretch',
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingBottom: 1,
+    alignItems: 'stretch',
   },
   toolbarButton: {
-      fontSize: 20,
-      width: 28,
-      height: 28,
-      textAlign: 'center'
+    fontSize: 20,
+    width: 28,
+    height: 28,
+    textAlign: 'center',
   },
   italicButton: {
-      fontStyle: 'italic'
+    fontStyle: 'italic',
   },
   boldButton: {
-      fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   underlineButton: {
-      textDecorationLine: 'underline'
+    textDecorationLine: 'underline',
   },
   lineThroughButton: {
-      textDecorationLine: 'line-through'
+    textDecorationLine: 'line-through',
   },
 });
