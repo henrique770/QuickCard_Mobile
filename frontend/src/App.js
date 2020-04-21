49c0145413993cccabc726f259cb2e758bc6f050
paulo <pauloh159@live.com>
2020-04-21 19:59:36 -0300

    Change to authentication flag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import createRouter from './routes';
 
+
 export default function App() {
-  //const signed = true;
-  const signed = useSelector(state => state.auth.signed);
+  const signed = true;
+  //const signed = useSelector(state => state.auth.signed);
   return createRouter(signed);
 }
