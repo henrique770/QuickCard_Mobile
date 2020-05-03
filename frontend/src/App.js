a72b5861991d6803acd31b39b4386f145ebdad4a
Pauloh Henrique <pauloh159@live.com>
2020-05-03 04:29:13 -0300

    Add logic when starting the application in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
-import createRouter from './routes';
+import React, {useState, useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 
+import createRouter from './routes';
+import { getDecks } from '~/store/modules/deck/actions'
 
 export default function App() {
   const signed = true;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+
+    if(signed)
+    {
+      dispatch(getDecks())
+    }
+  } , [])
+
   //const signed = useSelector(state => state.auth.signed);
   return createRouter(signed);
 }
