4397c7a0931b09e7a65bc62e8f747c5eb55b6b8d
Pauloh Henrique <pauloh159@live.com>
2020-05-17 21:27:29 -0300

    Proxy provider

--- frontend/src/store/service/serviceProxy.js
+++ frontend/src/store/service/serviceProxy.js
@@ -1,14 +1,5 @@
-import automapper from 'automapper-js'
-import Guid from 'guid'
-import api from '~/services/api'
-
 import { typeService } from './typeService'
-
-import factoryModel from '~/store/factory/factoryModel'
-import factoryEntity from '~/store/factory/factoryEntity'
-
 import RepositoryBase from '~/store/repository/repositoryBase'
-
 
 //#region
 
@@ -30,171 +21,93 @@ const _validators = {
 
 //#endregion
 
-class ServiceProxy {
+const ServiceProxy = (() => {
+
+  const state = {
+    add : [] ,
+    update : []
+  }
+  , eventOperation = {
+    add : 1 ,
+    update : 2
+  }
+
+  , repository = () => {}
+
+  , dispatch = () => {
+
+    console.log('state proxy' , state)
+  }
+    ,
 
     /**
-    * @param {TypeService} type
-    */
-    constructor(type)
-    {
-        _validators._ctr(type)
-
-        this._model = factoryModel.get(`${type}Model`)
-        this._entity = factoryEntity.get(`${type}Entity`)
-        this._repository = new RepositoryBase(this._model)
-        this._extds = undefined
-    }
-
-
-    //#region REPOSITORY OPERATION EXTENSION
-
-    include(type, prop, query) {
-
-      let self = Object.assign({}, this)
-
-      self.__proto__ = this.__proto__
-
-      if(self._extds == undefined)
-      {
-        self._extds = []
-      }
-
-      self._extds.push({
-        include : true
-        , include_args : {type, prop, query }
-      })
-
-      return self
-    }
-
-    _solveIncludeEntity(result, extds) {
-      let serviceProxyInclude = new ServiceProxy(extds.include_args.type)
-        , query_args = extds.include_args.query
-        , query = {
-          foryKey : query_args.foryKey
-          , operKey : query_args.operKey
-          , key : Array.isArray(result) ? result.map( e => e[query_args.foryKey] ) : [result[query_args.foryKey]]
+     *
+     * @param {String} operation
+     * @param {BaseEntity | BaseEntity[]} resource
+     * @return {BaseEntity | BaseEntity[]}
+     */
+   operationState = ( operation, resource ) => {
+     if(Array.isArray(resource)) {
+       state[operation].push(resource.map( ent => {
+        return {
+          id : ent.Id
+          , type : ent.constructor.name
         }
-        , key = query.operKey.split('_')[0]
-        , where = {}
-        , data = automapper( this._entity, result )
-
-      where[query_args.operKey] = query.key
-
-      return  serviceProxyInclude._repository
-        .query({
-          inn : query ,
-          where
-        })
-        .then( result => {
-
-          if(data == undefined)
-          {
-            return data
-          }
-          else if(Array.isArray(data))
-          {
-            for(let i = 0; i < data.length; i += 1)
-            {
-              let dataItem = data[i]
-                , relatedItens = result.filter( itemResult => itemResult[key] == dataItem[query.foryKey] )
-
-              if(relatedItens)
-              {
-                dataItem[extds.include_args.prop] = automapper( serviceProxyInclude._entity, relatedItens)
-              }
-            }
-          }
-          else {
-            let relatedItens = result.filter( itemResult => itemResult[key] == dataItem[query.foryKey] )
-            data[extds.include_args.prop] = automapper( serviceProxyInclude._entity, relatedItens)
-          }
-
-          return this._solveTypeResultOutPut(data)
-        })
+      }))
+    }
+    else {
+      state[operation].push({
+        id : resource.Id
+        , type : resource.constructor.name
+     })
     }
 
-    _solveTypeExtensionSwitch(result, extds) {
-      switch (true) {
+    dispatch()
 
-        case extds.include:
-          return this._solveIncludeEntity(result, extds)
+    return resource
+  }
 
-        default:
-          return result
-      }
-    }
+  , handler = {
+      /**
+       * proxy provider of get
+       * @param {RepositoryBase} target target object for proxy
+       * @param {String} key repository property
+       * @return { RepositoryBase | function(args) : BaseEntity }
+       */
+    get : (target , key) => {
+      console.log(`key target : ${key}`)
 
-    //#endregion
-
-    _solveTypeExtensionOutPut(result) {
-
-      if(this._extds != null && this._extds.length)
-      {
-        let extds = this._extds.pop()
-
-        return  this._solveTypeExtensionSwitch( result , extds )
-      }
-
-      return result
-    }
-
-    _solveTypeResultOutPut(result) {
-
-        if(this._extds != null && this._extds.length)
-        {
-            return this._solveTypeExtensionOutPut(result)
+      if(key === 'add') {
+        // replace target function
+        return (args) => {
+          return target.add(args)
+            .then( entity => operationState('add', entity))
         }
-
-        return automapper( this._entity, result )
-    }
-
-    //#region Repository read
-
-    async all() {
-
-        return this._repository.all()
-            .then( result => {
-               return this._solveTypeResultOutPut(result)
-            })
-    }
-
-    async where(query) {
-
-      let self = this
-
-      try {
-        return this._repository
-          .query(query)
-          .then( result => {
-
-            return self._solveTypeResultOutPut(result)
-          })
       }
-      catch (e) {
-        console.log('Error:' , e)
-        return { }
+
+      if(key === 'update') {
+        // replace target function
+        return (args) => {
+          return target.update(args)
+            .then( entity => operationState('update', entity))
+        }
+      }
+
+      if(target[key] !== undefined) {
+
+        return target[key]
       }
     }
+  }
 
-    //#endregion
+  return function (type) {
 
-    //#region Repository read
+    _validators._ctr(type)
 
-    async add(entitySource) {
+    const repository = new RepositoryBase(type)
 
-        let entityData = automapper(this._model, entitySource)
-          , self = this
-        entityData.Id = Guid.raw()
-        entityData.IsActive = true
-
-      return this._model.create(entityData)
-        .then( model => automapper(self._entity , model ))
-    }
-
-    //#endregion
-
-
-}
+    return new Proxy(repository, handler)
+  }
+})()
 
 export { ServiceProxy }
