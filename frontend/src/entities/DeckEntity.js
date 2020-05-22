51140801b040d25f5bf1c0739284994ea62535b1
Pauloh Henrique <pauloh159@live.com>
2020-05-22 01:52:21 -0300

    Logic entity Deck. Check if the Deck is empty (No cards)

--- frontend/src/entities/DeckEntity.js
+++ frontend/src/entities/DeckEntity.js
@@ -3,24 +3,54 @@ import BaseEntity  from './BaseEntity'
 
 class DeckEntity extends BaseEntity {
 
-    constructor(args = {})
-    {
-      super(args)
+   constructor(args = {})
+   {
+    super(args)
 
-      this._name = args.Name
-      this._cards = args.Cards
-    }
+    this._name = args.Name
+    this._cards = args.Cards
+   }
 
-    get Name(){ return this._name }
-    set Name(value) { this._name = value}
+   get Name(){ return this._name }
+   set Name(value) { this._name = value}
 
-    get Cards() {
+  /**
+   * returns card related to the deck
+   * @return {Cards[]}
+   * @constructor
+   */
+  get Cards() {
       if(this._cards === undefined)
         return []
 
-      return  this._cards
+      return this._cards
     }
-    set Cards(value) { return this._cards = value}
+
+  /**
+   * insert card array to deck
+   * @param value {Cards[]} - cards list
+   * @constructor
+   */
+    set Cards(value) {
+
+      if(this._cards === undefined)
+        this._cards = []
+
+      if (Array.isArray(value)) {
+        this._cards.push(...value)
+      }
+      else if(typeof value === 'object') {
+        this._cards.push(push)
+      }
+    }
+
+  /**
+   * check for cards on the deck
+   * @return {boolean}
+   */
+  isEmpty() {
+      return this.Cards.length === 0
+    }
 
     getDeckRandom() {
       let countCards = this.Cards.length
