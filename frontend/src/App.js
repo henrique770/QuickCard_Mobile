ec4bc324e7f620ae2ddc18eced1f99ddee1d67f4
Pauloh Henrique <pauloh159@live.com>
2020-06-05 01:27:27 -0300

    initial application rule of application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,10 @@ import React, {useState, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 
 import createRouter from './routes';
-import { getDecks } from '~/store/modules/deck/actions'
 
 export default function App() {
   const signed = true;
-  const dispatch = useDispatch();
-
   useEffect(() => {
-
-    if(signed)
-    {
-      dispatch(getDecks())
-    }
   } , [])
 
   //const signed = useSelector(state => state.auth.signed);
