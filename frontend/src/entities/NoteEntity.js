6f0200f964a24abba91701f0e5bfd6b9ee246ad0
Pauloh Henrique <pauloh159@live.com>
2020-06-17 00:20:57 -0300

    modifications to entities for annotation block

--- frontend/src/entities/NoteEntity.js
+++ frontend/src/entities/NoteEntity.js
@@ -7,6 +7,19 @@ import BaseEntity from '~/entities/BaseEntity';
 class NoteEntity extends BaseEntity {
   constructor(args = {}) {
     super(args);
+
+    this.Content = args.Content;
+    this.IdNotePad = args.IdNotePad;
+  }
+
+  get Content() { return this._content; }
+  set Content(value) {
+    this._content = value;
+  }
+
+  get IdNotePad() { return this._idNotePad; }
+  set IdNotePad(value) {
+    this._idNotePad = value;
   }
 }
 
--- frontend/src/entities/NotePadEntity.js
+++ frontend/src/entities/NotePadEntity.js
@@ -10,8 +10,6 @@ class NotePadEntity extends BaseEntity {
 
     this._name = args.Name;
     this._notes = args.Notes;
-
-    this.orderNotes();
   }
 
   get Name() {
@@ -21,70 +19,15 @@ class NotePadEntity extends BaseEntity {
     this._name = value;
   }
 
-  /**
-   * returns note related to the deck
-   * @return {Note[]}
-   * @constructor
-   */
   get Notes() {
-    if (this._notes === undefined) return [];
-
-    return this._notes.filter(e => e.IsActive);
+    return this._notes;
   }
-
-  /**
-   * insert note array to deck
-   * @param value {Note[]} - Notes list
-   * @constructor
-   */
   set Notes(value) {
-    if (this._notes === undefined) this._notes = [];
-
-    this._notes = value.filter(e => e.IsActive);
+    this._notes = value;
   }
 
-  /**
-   * check for Notes on the deck
-   * @return {boolean}
-   */
-  isEmpty() {
-    return this.totalNotes() === 0;
-  }
-
-  orderNotes() {
-    if (this.totalNotes() === 0) return;
-
-    let order = (a, b) => {
-        if (a.DateNextView !== undefined && b.DateNextView !== undefined)
-          return new Date(a.DateNextView) < new Date(b.DateNextView);
-
-        return false;
-      },
-      NotesOrder = this.Notes.sort(order);
-
-    this.Notes = NotesOrder;
-  }
-
-  /**
-   * total Notes in deck
-   * @return {number}
-   */
-  totalNotes() {
+  get totalNotes() {
     return this.Notes.length;
-  }
-
-  removeNote(note) {
-    let indexOf = this.Notes.map(note => note.Id).indexOf(note.Id);
-
-    if (indexOf > -1) {
-      this.Notes.splice(indexOf, 1);
-      return true;
-    }
-    return false;
-  }
-
-  addNote(note) {
-    this.Notes.push(note);
   }
 }
 
