0c4090196226ace6f47d4dd223689486d0c6c18b
Paulo Henrique <pauloh159@live.com>
2020-08-26 00:45:45 -0300

    configuration url to API horuku

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://177.159.255.212:3000/api/v1',
+  baseURL: 'https://quickcard-io.herokuapp.com/api/v1/',
 });
 
 export default api;
